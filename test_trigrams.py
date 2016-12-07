f1bf1d31a62228013c98238c6c7fabd9c41d2527
Rick Valenzuela <rv@rickv.com>
2016-12-07 14:35:41 -0800

    Fixed unclosed bracket

--- test_trigrams.py
+++ test_trigrams.py
@@ -3,20 +3,19 @@
 import pytest
 
 READ_EVAL_TABLE = [
-    ["I may I wish I may I wish I might", 3, ["I", "wish", "may"]],
-    ["Do that voodoo that you do so well.", 4, ["voodoo", "that", "you", "do"]]
+    ['testtext.txt', ['Do', 'the', 'voodoo', 'that', 'you', 'do', 'so', 'well.']]
 ]
 
 
-@pytest.mark.parametrize('str, answer')
-def test_read_file(str):
+@pytest.mark.parametrize('file, answer', READ_EVAL_TABLE)
+def test_read_file(file, answer):
     """Test to ensure function is reading text."""
     from trigrams import read_file
-    assert read_file("This is a string") == ["this", "is", "a", "string"]
+    assert read_file(file) == answer
 
 
-@pytest.mark.parametrize('str, n, result', READ_EVAL_TABLE)
-def test_read_eval(str, n, result):
-    """Test to ensure textfile is read."""
-    from trigrams import read_file
-    assert read_file(str, n) == result
+# @pytest.mark.parametrize('str, n, result', READ_EVAL_TABLE)
+# def test_read_eval(str, n, result):
+#     """Test to ensure textfile is read."""
+#     from trigrams import read_file
+#     assert read_file(str, n) == result
