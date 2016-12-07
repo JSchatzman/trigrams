180a2d9260b3455ae57c8ac89791de54c4e119d1
Rick Valenzuela <rv@rickv.com>
2016-12-07 13:55:36 -0800

    Redid readfile test

---
+++ test_trigrams.py
@@ -0,0 +1,22 @@
+"""Tests for trigrams."""
+
+import pytest
+
+READ_EVAL_TABLE = [
+    ["I may I wish I may I wish I might", 3, ["I", "wish", "may"]]
+    ["Do that voodoo that you do so well.", 4, ["voodoo", "that", "you", "do"]]
+]
+
+
+@pytest.mark.parametrize('str, answer')
+def test_read_file(str):
+    """Test to ensure function is reading text."""
+    from trigrams import read_file
+    assert read_file("This is a string") == ["this", "is", "a", "string"]
+
+
+@pytest.mark.parametrize('str, n, result', READ_EVAL_TABLE)
+def test_read_eval(str, n, result):
+    """Test to ensure textfile is read."""
+    from trigrams import read_file
+    assert read_file(str, n) == result
