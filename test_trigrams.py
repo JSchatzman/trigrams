c0223782a62f77ebc8def8e0a52fd4b962722fe5
Rick Valenzuela <rv@rickv.com>
2016-12-07 16:13:18 -0800

    Changed dict test input param to file

--- test_trigrams.py
+++ test_trigrams.py
@@ -12,9 +12,9 @@ READ_FILE_TABLE = [
 
 DICT_TABLE = [
 
-        [['Do', 'the', 'voodoo', 'that', 'you', 'do', 'so', 'well.'],
+        ['testtext.txt',
         {'Do the': ['voodoo'], 'the voodoo': ['that'], 'voodoo that': ['you'], 'that you': ['do'], 'you do': ['so'], 'do so': ['well.']}],
-        [['Hark!', 'Who', 'goes', 'there?', "'Tis", 'I,', 'some', 'student', 'in', 'Python', '401.', 'Oh.', 'OK,', 'then.', "C'mon", 'in.'],
+        ['dict_testtext.txt',
         {'Hark! Who': ['goes'], 'Who goes': ['there?'], 'goes there?': ["'Tis"], "there? 'Tis": ['I,'], "'Tis I,": ['some'], 'I, some': ['student'], 'some student': ['in'], 'student in': ['Python'], 'in Python': ['401.'], 'Python 401.': ['Oh.'], '401. Oh.': ['OK,'], 'Oh. OK,': ['then.'], 'OK, then.': ["C'mon"], "then. C'mon": ['in.']}]
 
 ]
@@ -32,3 +32,4 @@ def test_create_dict(word_list, result):
     """Test to ensure textfile is read."""
     from trigrams import create_dict
     assert create_dict(word_list) == result
+    
