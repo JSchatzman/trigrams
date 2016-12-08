ac0c66074aedeec87bf3eb52e1472f7db9ea5ae3
Rick Valenzuela <rv@rickv.com>
2016-12-07 16:41:44 -0800

    Refactored read test, fixed formatting

--- test_trigrams.py
+++ test_trigrams.py
@@ -2,29 +2,42 @@
 
 import pytest
 
-READ_FILE_TABLE = [
-    [
-        'testtext.txt',
-        ['Do', 'the', 'voodoo', 'that', 'you', 'do', 'so', 'well.']
-    ]
-]
-
 
 DICT_TABLE = [
 
-        ['testtext.txt',
-        {'Do the': ['voodoo'], 'the voodoo': ['that'], 'voodoo that': ['you'], 'that you': ['do'], 'you do': ['so'], 'do so': ['well.']}],
-        ['dict_testtext.txt',
-        {'Hark! Who': ['goes'], 'Who goes': ['there?'], 'goes there?': ["'Tis"], "there? 'Tis": ['I,'], "'Tis I,": ['some'], 'I, some': ['student'], 'some student': ['in'], 'student in': ['Python'], 'in Python': ['401.'], 'Python 401.': ['Oh.'], '401. Oh.': ['OK,'], 'Oh. OK,': ['then.'], 'OK, then.': ["C'mon"], "then. C'mon": ['in.']}]
+    ['testtext.txt',
+        {
+            'Do the': ['voodoo'],
+            'the voodoo': ['that'],
+            'voodoo that': ['you'],
+            'that you': ['do'],
+            'you do': ['so'],
+            'do so': ['well.']
+        }],
 
+    ['dict_testtext.txt',
+        {
+            'Hark! Who': ['goes'],
+            'Who goes': ['there?'],
+            'goes there?': ["'Tis"],
+            "there? 'Tis": ['I,'],
+            "'Tis I,": ['some'],
+            'I, some': ['student'],
+            'some student': ['in'],
+            'student in': ['Python'],
+            'in Python': ['401.'],
+            'Python 401.': ['Oh.'],
+            '401. Oh.': ['OK,'],
+            'Oh. OK,': ['then.'],
+            'OK, then.': ["C'mon"],
+            "then. C'mon": ['in.']}]
 ]
 
 
-@pytest.mark.parametrize('file, answer', READ_FILE_TABLE)
-def test_read_file(file, answer):
+def test_read_file():
     """Test to ensure function is reading text."""
     from trigrams import read_file
-    assert read_file(file) == answer
+    assert read_file('testtext.txt') == ['Do', 'the', 'voodoo', 'that', 'you', 'do', 'so', 'well.']
 
 
 @pytest.mark.parametrize('word_list, result', DICT_TABLE)
@@ -32,4 +45,10 @@ def test_create_dict(word_list, result):
     """Test to ensure textfile is read."""
     from trigrams import create_dict
     assert create_dict(word_list) == result
-    
+
+
+# @pytest.mark.parametrize('word_list, result', DICT_TABLE)
+# def test_create_dict(word_list, result):
+#     """Test to ensure textfile is read."""
+#     from trigrams import create_dict
+#     assert create_dict(word_list) == result
